0ec36297f2550dba3fe1b9a1e060e881d0c926f7
radik.kriolampah <radik.kriolampah@gmail.com>
2021-06-29 08:04:33 +0700

    initial commit;

---
+++ media/text/strings.js
@@ -0,0 +1,58 @@
+var _STRINGS = {
+	"Ad":{
+		"Mobile":{
+			"Preroll":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"Header":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"End":{
+				"ReadyIn":"Advertisement ends in ",
+				"Loading":"Please wait ...",
+				"Close":"Close",
+			},
+		},
+	},
+
+	"Splash":{
+		"Loading":"Loading",
+		"TapToStart" : "TAP TO START"				
+	},
+
+	"Menu":{
+		"ButtonEasy":"EASY",
+		"ButtonNormal":"NORMAL",
+		"ButtonDifficult":"DIFFICULT",
+		"PopUpSettingsTitle":"SETTINGS",
+		"GameMode":"GAME MODE :",
+		"GameModeClassic":"CLASSIC",
+		"GameModeTimed":"TIMED",
+		"GameModeOptions":"LENGTH OF GAME",
+		"GameModeOptionsClassic": "(POINTS) :",
+		"GameModeOptionsTimed": "(MINUTES) :",
+		"TutorialQuestion": "Play Tutorial ?",
+		"Yes": "Yes",
+		"No": "No",
+		"SetGameMode": "Click this button to setup game mode !"
+	},
+
+	"Game":{
+		"PopUpPauseTitle":"PAUSE",
+		"ResultTitle":"GAME RESULT",
+		"ResultWin":"YOU WIN",
+		"ResultLose":"YOU LOSE",
+		"ResultDraw":"DRAW",
+		"Goal":"GOAL",
+		"Tutorial1":"Drag the paddle to move",
+		"Tutorial2":"Hit the puck with the paddle to shoot or defend",
+		"Tutorial3":"Are you ready ?",
+		"ButtonReady":"Ready",
+		"ButtonNext":"Next"
+	}
+
+};
